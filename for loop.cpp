5691f4e68923c44b0652fc7750842b25cc17e432
fachry99 <72313995+fachry99@users.noreply.github.com>
2021-08-18 09:18:28 +0700

    Add files via upload

---
+++ for loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << " Start \n";
+	for(int i = 1; i <= 11; i++){
+		cout << i << endl;
+	}
+	cout << "program selesai \n";
+	
+	return 0;
+}
+
+
